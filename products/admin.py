57ce9bded5e33860b2b1ff8ce8a6da128da80090
brendan o brien <brenande@gmail.com>
2019-09-22 22:01:00 +0100

    Added new app to project

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Aid
+from .models import Aid, Product
 
 # Register your models here.
 admin.site.register(Aid)
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -2,12 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-class Aid(models.Model):
+class Aid(models.Model): 
     brand = models.CharField(max_length=254, default='')
     model_type = models.TextField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.brand
+
+
+class Product(models.Model):
+    brand = models.CharField(max_length=254, default='')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='images')
+    isAccessory = models.BooleanField(default=False)
 
     def __str__(self):
         return self.brand
