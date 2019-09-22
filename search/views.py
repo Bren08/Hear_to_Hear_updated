669b987fecfa4dbcf214d0771fed3b068ce052df
brendan o brien <brenande@gmail.com>
2019-09-22 22:02:04 +0100

    Renamed search function

--- search/views.py
+++ search/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from products.models import Aid
 
+
 # Create your views here.
-def do_search(request):
-    products = Aid.objects.filter(name__icontains=request.GET['q'])
+def search_products(request):
+    products = Aid.objects.filter(brand__icontains=request.GET['q'])
     return render(request, "products.html", {"products": products})
+
