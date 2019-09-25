323822d0e2305dde2ce131acc48a84392d0fb592
brendan o brien <brenande@gmail.com>
2019-09-25 06:11:11 +0100

    App crashes when DEBUG set to False?

--- hear_to_hear/settings.py
+++ hear_to_hear/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+
+DEBUG = True ##Application crashes if set to false
 
 ALLOWED_HOSTS = ['hear-to-hear.herokuapp.com', '127.0.0.1']
 
